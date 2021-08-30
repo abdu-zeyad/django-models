8410b2662ab4936e00f030690c40c503729a8ee3
Abdelmajed-zeyad <abd.d@live.com>
2021-08-30 22:41:27 +0300

    s

--- snack_tracker_project/settings.py
+++ snack_tracker_project/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -37,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "snacks",
+    "snacks.apps.SnacksConfig",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +53,7 @@ ROOT_URLCONF = "snack_tracker_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": BASE_DIR / "templates",
+        "DIRS": "templates",
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -69,7 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "snack_tracker_project.wsgi.application"
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases

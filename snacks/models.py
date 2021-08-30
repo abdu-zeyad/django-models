6cb9b454e9a1b51cd373af87f40ce8dd652e61ec
Abdelmajed-zeyad <abd.d@live.com>
2021-08-30 22:17:36 +0300

    add templaete

--- snacks/models.py
+++ snacks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class Snacks(models.Model):
     name = models.CharField(max_length=64)
     purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    description = models.TextField()
+    description = models.TextField(default=0)
 
     def __str__(self):
         return self.name

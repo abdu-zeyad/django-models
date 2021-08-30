74e073a5c0c0b7e2d1150211380529ac80183985
Abdelmajed-zeyad <abd.d@live.com>
2021-08-30 22:09:14 +0300

    changes

--- snacks/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SnackListView
-
-urlpatterns = [
-    path("", SnackListView.as_view(), name="snack_list"),
-    path("<int:pk>/", SnackListView.as_view(), name="snack_detail"),
-]

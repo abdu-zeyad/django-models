3a2b5a116cf480282e89ee7e6c5ee8a8e11ef359
Abdelmajed-zeyad <abd.d@live.com>
2021-08-31 12:22:32 +0300

    redo the lab

--- snacks/admin.py
+++ snacks/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import Snacks
 
-admin.site.register(Snacks)
+# Register your models here.
--- snacks/views.py
+++ snacks/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Snacks
 
-
-class SnackListView(ListView):
-    template_name = "snack_list.html"
-    model = Snacks
-
-
-class SnackDetailView(DetailView):
-    template_name = "snack_detail.html"
-    model = Snacks
+# Create your views here.
